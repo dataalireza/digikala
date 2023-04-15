3e2057cbee2a69609a79530753366b30e0561f06
dataalireza <alirezajafarijp1@gmail.com>
2023-04-15 21:07:15 +0330

    project

---
+++ js/main.js
@@ -0,0 +1,56 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+var amazing = new Swiper(".myamazing", {
+  slidesPerView: 6,
+  spaceBetween: 2,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+var brand = new Swiper(".mybrand", {
+  slidesPerView: 9,
+  spaceBetween: 1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+var best = new Swiper(".mybest", {
+  slidesPerView: 4,
+  spaceBetween: 0.5,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
